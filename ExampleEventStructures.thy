7c3df0fe904e2daf04d124822dc31670ccbcc4de
jon <js942@kent.ac.uk>
2017-02-21 13:11:00 +0000

    Creates separate file for example event structures and proofs and adds the event structure for java causality test case 6

---
+++ ExampleEventStructures.thy
@@ -0,0 +1,54 @@
+theory ExampleEventStructures
+imports EventStructures
+begin
+
+theorem "well_justified empty_ES"
+apply (auto simp add: well_justified_def justified_def empty_ES_def)
+apply (rule refl)
+done
+
+definition ex1 :: "nat event_structure_data" where
+"ex1 \<equiv> 
+  \<lparr> event_set = {30, 31, 32, 33, 34, 35, 36, 37, 38},
+  partial_order = \<lambda>x y. (x,y) \<in>
+   {(30,31), (30,32), (30, 33)},
+  primitive_conflict = \<lambda>x y. (y,x) \<in> {(31,35), (33,34)},
+  label_function = \<lambda>x.
+    if x = 30 then 
+      Label I '''' 0
+    else
+      Label R ''x'' 0 \<rparr>"
+
+
+theorem "well_justified ex1"
+apply(auto simp add: well_justified_def)
+apply(auto simp add: justified_def)
+apply(auto simp add: empty_ES_def)
+
+apply(auto simp add: label_function_def)
+apply(case_tac event_structure_data_ext_Tuple_Iso)
+apply(case_tac getMemAction_cases)
+oops
+
+(*Java Causality Test Case 6 Event Structure
+  Should be accepted by model*)
+definition jctc6_ES :: "nat event_structure_data" where
+"jctc6_ES \<equiv> \<lparr> 
+  event_set = {0, 1, 2, 3, 4, 5, 6, 7},
+  partial_order = \<lambda>x y. (x,y) \<in>
+    {(0,1), (0,2), (0, 3), (0, 34), (2,5), (3,6), (4,7)},
+  primitive_conflict = \<lambda>x y. (y,x) \<in> {(1,2), (3,4)},
+  label_function = \<lambda>x.
+    if x = 0 then Label I '''' 0
+    else if x = 1 then Label R ''x'' 0
+    else if x = 2 then Label R ''x'' 1
+    else if x = 3 then Label R ''y'' 0
+    else if x = 4 then Label R ''y'' 1
+    else if x = 5 then Label W ''x'' 1
+    else if x = 6 then Label W ''x'' 0
+    else Label W ''x'' 1
+\<rparr>"
+
+
+
+end
