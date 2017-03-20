d96a965b6706e2163d36f3e40f607729e5c5b496
Simon Cooksey <sjc205@kent.ac.uk>
2017-03-20 17:56:18 +0000

    Remove useless lemmas from ExampleEventStructures.thy

--- ExampleEventStructures.thy
+++ ExampleEventStructures.thy
@@ -1,16 +1,6 @@
 theory ExampleEventStructures
 imports Enum EventStructures Relation Transitive_Closure String
 begin
-  
-value "refl {}"
-value "top"
-  
-theorem "isValidPO {} {}"
-  apply(simp add: isValidPO_def)
-  oops
-    
-schematic_goal ex: "acyclic {(''a'', ''a'')} \<Longrightarrow> antisym ({(''a'', ''a'')}\<^sup>*)"
-  by (rule Transitive_Closure.acyclic_impl_antisym_rtrancl)
 
 theorem emptyESValidES: "isValidES empty_ES"
   apply(auto simp add: isValidES_def empty_ES_def isValidPO_def)
