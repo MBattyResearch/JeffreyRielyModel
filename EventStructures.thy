8c667eed901df6556ed1c92596611ec26af1f70f
Simon Cooksey <sjc205@kent.ac.uk>
2017-05-15 13:57:02 +0100

    Added powerset of configurations constraint to model

--- EventStructures.thy
+++ EventStructures.thy
@@ -5,7 +5,6 @@ theory EventStructures
 imports Main Relation Transitive_Closure RelationalLemmas EventStructures2
 begin
   
-
 (*text{* Primitive conflict has no edges in it which can be inferred by the rule 
 @{thm [source] confOverPO_def}. *}
 definition minimal :: "'a rel \<Rightarrow> 'a rel \<Rightarrow> bool" where
@@ -39,12 +38,6 @@ definition isValidES :: "'a event_structure \<Rightarrow> bool" where
   ((symmetriccl (primitive_conflict es))\<^sup>+ - Id)"
 *)
 
-(* FIXME 
-function justifies_event :: "label set \<Rightarrow> label rel" where
-"justifies_event evs \<equiv> 
-  { (w, r) . (w \<in> evs) \<and> (r \<in> evs) \<and> (\<exists>l l2 v v2 . ((w = (Label W l v)) \<and> (r = (Label R l2 v2)) \<and> (l = l2) \<and> (v = v2))) } \<union>
-  { (i, r) . (i \<in> evs) \<and> (r \<in> evs) \<and> (\<exists>v . (i = (Label I '''' _)) \<and> (r = (Label R _ v)) \<and> (v = 0))}"
-*)
 fun justifies_event :: "label \<Rightarrow> label \<Rightarrow> bool" where
 "justifies_event (Label I '''' v) (Label R l2 v2) = (v2 = 0)"|
 "justifies_event (Label W l v) (Label R l2 v2) = ((l = l2) \<and> (v = v2))"|
@@ -59,32 +52,25 @@ definition conflict_free :: "'a config \<Rightarrow> 'a rel \<Rightarrow> bool" 
 fun getMemAction :: "label \<Rightarrow> mem_action" where
 "getMemAction (Label x _ _) = x"
 
-(*locale model = labelledES*)
-(*  fixes es :: "'a event_structure"*)
-(*    assumes validity: "isValidES es"*)
-    
-
 context labelledES
   begin     
     (*All read events in a configuration are justified by an event in that configuration*)
     definition justified :: "'a config \<Rightarrow> bool" where
     "justified c \<equiv>
-       (\<forall>r\<in> c. (getMemAction (label_function es r) = R) \<longrightarrow> 
-        (\<exists>e\<in>c. (justifies_event (label_function es e) (label_function es r))))"
+       (\<forall>r\<in> c. (getMemAction (label r) = R) \<longrightarrow> 
+        (\<exists>e\<in>c. (justifies_event (label e) (label r))))"
     
     definition is_read :: "'a \<Rightarrow> bool" where
-      "is_read x \<equiv> (getMemAction (label_function es x) = R)"
+      "is_read x \<equiv> (getMemAction (label x) = R)"
 
     definition justifies_config :: "'a config \<Rightarrow> 'a config \<Rightarrow> bool"  where
-      "justifies_config A B \<equiv> \<forall>x \<in> B. is_read x \<longrightarrow> (\<exists>y \<in> A. justifies_event (label_function es y) (label_function es x))"
+      "justifies_config A B \<equiv> \<forall>x \<in> B. is_read x \<longrightarrow> (\<exists>y \<in> A. justifies_event (label y) (label x))"
 
     definition justifies_config_subset :: "'a config \<Rightarrow> 'a config \<Rightarrow> bool" (infixl "\<lesssim>" 60) where
       "justifies_config_subset A B  \<equiv> A \<subseteq> B \<and> justifies_config A B"
 
     definition justifies_config_star ::  "'a config \<Rightarrow> 'a config \<Rightarrow> bool" (infixl "\<lesssim>\<^sup>*" 60) where
         "justifies_config_star a b \<equiv> justifies_config_subset\<^sup>*\<^sup>* a b"
-
-value \<CC>
       
     definition ae_justifies :: "'a config  \<Rightarrow> 'a config \<Rightarrow> bool" where
       "ae_justifies C D \<equiv> \<forall>C' \<in> \<CC>. (C \<lesssim>\<^sup>* C') \<longrightarrow> (\<exists>C'' \<in> \<CC>. (C' \<lesssim>\<^sup>* C'') \<and> justifies_config C'' D)"
@@ -98,21 +84,10 @@ value \<CC>
     definition well_justified :: "'a config  \<Rightarrow> bool" where
       "well_justified C \<equiv> (justified C) \<and> C \<in> \<CC> \<and> {}\<sqsubseteq>\<^sup>*C"
     
-    definition initialES :: "nat event_structure" ("\<I>" 1000) where
-    "initialES \<equiv> \<lparr> 
-      event_set = {},
-      primitive_order ={},
-      primitive_conflict = {},
-      label_function = \<lambda>x.(Label I '''' 0)
-    \<rparr>"
-    
     definition initialConfig :: "nat set" ("\<C>" 1000) where
       "initialConfig \<equiv> {}"
     
     declare initialConfig_def [simp]
-    end
-
-locale config = event_structures +
- 
+  end
 
 end
--- EventStructures2.thy
+++ EventStructures2.thy
@@ -4,7 +4,7 @@ begin
 
 locale partial_order =
   fixes preceeds :: "'a \<Rightarrow> 'a \<Rightarrow> bool" (infixl "\<preceq>" 50)
-  fixes events :: "'a set"
+  fixes events :: "'a set" ("\<EE>" 1000)
   assumes refl [intro, simp]: "x \<preceq> x"
     and anti_sym [intro]: "\<lbrakk> x \<preceq> y; y \<preceq> x \<rbrakk> \<Longrightarrow> x = y"
     and trans [trans]: "\<lbrakk> x \<preceq> y; y \<preceq> z \<rbrakk> \<Longrightarrow> x \<preceq> z"
@@ -16,44 +16,15 @@ locale primeES = partial_order +
 
 datatype mem_action = W | R | I   
 datatype label = Label mem_action string int
+type_synonym 'a config = "'a set"
 
 locale labelledES = primeES + 
   fixes label :: "'a \<Rightarrow> label"
-  fixes config_domain :: "'a set set"
+  fixes config_domain :: "'a config set" ("\<CC>" 1000)
   assumes conflict_free: "\<lbrakk>C \<in> config_domain; x \<in> C; y \<in> C \<rbrakk> \<Longrightarrow> \<not>(conflict x y)"
     and down_closed: "\<lbrakk>C \<in> config_domain; y \<in> C; x \<preceq> y \<rbrakk> \<Longrightarrow> x \<in> C"
-    and config_subset: "C \<in> config_domai \<Longrightarrow> C \<subseteq> events"
+    and config_subset: "C \<in> \<CC> \<Longrightarrow> C \<subseteq> \<EE>"
 
 
-
-
-
-
-type_synonym 'a configuration = "'a set"
-
-
-context labelledES
-begin
-
-
-axiomatization where
-    down_closed: "\<forall>C::'a configuration. y \<in> C \<Longrightarrow> x = y \<Longrightarrow> x \<in> C"
-and conflict_free: "\<forall>C::'a configuration. x \<in> C \<and> y \<in> C \<Longrightarrow> \<not>(x = y)"
-
-
-
-end
-
-(*
-locale configuration = primeES + 
-  assumes conflict_free: "\<lbrakk> x \<in> events; y \<in> events \<rbrakk> \<Longrightarrow> \<not>(conflict x y)"
-    and down_closed: "\<lbrakk>y \<in> events; x \<preceq> y \<rbrakk> \<Longrightarrow> x \<in> events"
-
-context labelledES
-begin
-    definition config_domain :: "'a set" where 
-    "config_domain \<equiv> {C . C \<subseteq> events \<and> down_closed events precedes \<and> conflict_free events conflict}"
-end
-*)
 
 end
